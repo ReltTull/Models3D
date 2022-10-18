e1eece31ce93120ce4f41b03a7e9eb05ce04f53f
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-10-18 19:08:06 +0300

    ModelStore upd

--- InMemoryModel/ModelStore.java
+++ InMemoryModel/ModelStore.java
@@ -2,19 +2,34 @@ package Models3D.InMemoryModel;
 
 import Models3D.ModelElements.Camera;
 import Models3D.ModelElements.Flash;
+import Models3D.ModelElements.PoligonalModelStaff.Poligon;
 import Models3D.ModelElements.PoligonalModelStaff.PoligonalModel;
 import Models3D.ModelElements.Scene;
+import Models3D.Plugs.Angle3D;
+import Models3D.Plugs.Point3D;
+import java.util.ArrayList;
 
 public class ModelStore
 {
-    public PoligonalModel model;
-    public Scene scene;
-    public Flash flash;
-    public Camera camera;
+    public ArrayList<PoligonalModel> models = new ArrayList<>();
+    public ArrayList<Scene> scenes = new ArrayList<>();
+    public ArrayList<Flash> flashes = new ArrayList<>();
+    public ArrayList<Camera> cameras = new ArrayList<>();
+    private ArrayList<IModelChangedObserver> changedObservers = new ArrayList<>();
+
+    public ModelStore(Poligon poligon)
+    {
+        this.models.add(new PoligonalModel(poligon));
+        this.scenes.add(new Scene<>(0, models.get(0), cameras.get(0)));
+        this.flashes.add(new Flash());
+        this.cameras.add(new Camera(new Point3D(), new Angle3D()));
+    }
+
+    public void notifyChange (IModelChanger sender) {
+    }
 
     public Scene getScene (int value)
     {
-        Scene scn = new Scene();
-        return scn;
+        return new Scene(value, models.get(value), cameras.get(value));
     }
 }
