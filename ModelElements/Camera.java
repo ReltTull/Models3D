cd7bd01af007679b6de1ae9a85b12178018f3e08
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-10-18 19:03:59 +0300

    camera upd

--- ModelElements/Camera.java
+++ ModelElements/Camera.java
@@ -8,15 +8,16 @@ public class Camera
     public Point3D location;
     public Angle3D angle;
 
-    public void rotate(Angle3D input) {
-    }
-
     public Camera(Point3D location, Angle3D angle) {
         this.location = location;
         this.angle = angle;
     }
 
-    public void move(Point3D input) {
+    public void rotate(Angle3D input)
+    { // вращение камеры
+    }
 
+    public void move(Point3D input)
+    { // перемещение камеры
     }
 }
